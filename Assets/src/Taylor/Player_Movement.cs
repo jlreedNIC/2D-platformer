fb9166cad53beca59afe58d90b6c48b0f3d09a66
TaylorM <tmartin6894@gmail.com>
2022-09-09 16:12:53 -0700

    added player movement script. Can move left, right, and jump

---
+++ Assets/src/Taylor/Player_Movement.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    //these will be our base player movement speed and jump height. 
+    //serialized field lets us have private variables vieded and edited in Unity editor 
+    [SerializeField] private float _playerMovementSpeed = 5.0f; 
+    [SerializeField] private float _playerJumpHeight = 5.0f; 
+    [SerializeField] private Transform _groundChecker; 
+    [SerializeField] private float _groundCheckerRadius; 
+    [SerializeField] private LayerMask _groundLayer;  
+    [SerializeField] private bool _isTouchingGround;  
+
+
+    //player rigidbody
+    [SerializeField] private Rigidbody2D _playerRB; 
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //grab the RigidBody2D component for movement
+        _playerRB = GetComponent<Rigidbody2D>(); 
+
+        //null game object check
+        if(_playerRB == null)
+        {
+            Debug.Log("Player is Missing RigidBody2D");
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //get input and move player
+        MovePlayer(); 
+
+        if(Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            Jump();
+        }
+    }
+
+    //using unity's expression method, we can make a clean one line function.
+    // private void Jump() => _playerRB.velocity = new Vector2(0 , _playerJumpHeight); 
+
+    //However to follow coding standards, we use brackets even for one liners
+    //function to make player jump
+    private void Jump()
+    {
+        _playerRB.velocity = new Vector2(_playerRB.velocity.x, _playerJumpHeight); 
+    }
+
+    //function to check if the player is on level terrain
+    private bool IsGrounded()
+    {
+        return _isTouchingGround = Physics2D.OverlapCircle(_groundChecker.position,_groundCheckerRadius, _groundLayer); 
+    }
+
+    //horizontal player movement function 
+    private void MovePlayer()
+    {
+        //using the unity input manager
+        //grab the horizontal axis input. 
+        //this will be the left and right arrows on a keyboard
+        var horizontalMovementInput = Input.GetAxisRaw("Horizontal"); 
+
+        //move the player
+        _playerRB.velocity = new Vector2(horizontalMovementInput * _playerMovementSpeed, _playerRB.velocity.y); 
+    }
+}
