4f4aaa34305770f5868599fda2f809d970bbf12c
Jordan Reed <reed5204@vandals.uidaho.edu>
2022-09-10 11:12:12 -0700

    updated item and gamemanager scripts. UpdateHealth function caps at maxhealth and 0. Health now shows a heart counter.

--- Assets/src/Jordan/GameManager.cs
+++ Assets/src/Jordan/GameManager.cs
@@ -1,7 +1,12 @@
-/*
-class to manage score, maybe health
-also manages drbc death count
-*/
+/**
+ * @file    GameManager.cs
+ * @date    09-10-22
+ * @class   CS 383
+ * @project 2D-platformer
+ * @brief   This file manages the score, health, and the general in-game overlay.
+ *          It also manages the drbc death count.
+ * 
+ */
 
 using System.Collections;
 using System.Collections.Generic;
@@ -12,42 +17,47 @@ public class GameManager : MonoBehaviour
 {
     public static int score = 0;    // look up difference between public static and private in C#/unity
     
-    public static int health = 3;   // should health be here? or with player script
+    public static int health = 3;
+    public static int maxHealth = 3;
 
-    public int BCdeathCount = 0;    // set during menu
-    private bool bcmode = false;
+    public int BCdeathCount = 0;
+    private bool bcmode = false;    // set during start menu
 
     // updating score on screen
     public GameObject game_overlay;
     Text scoreText;                 // text object for score
     Text healthText;                // text object for health
-                                    // used for debugging purposes for now
 
     // Start is called before the first frame update
     void Start()
     {
-        game_overlay = GameObject.Find("ScoreText"); // find the score text object
-        scoreText = game_overlay.GetComponent<Text>(); // for updating the score string
+        // get scoreText object and get text component
+        game_overlay = GameObject.Find("ScoreText");
+        scoreText = game_overlay.GetComponent<Text>();
 
-        // reuse gameoverlay variable
-        game_overlay = GameObject.Find("HealthText"); // only used in debugging!
+        // get healthText object and get text component
+        game_overlay = GameObject.Find("HealthText");
         healthText = game_overlay.GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // update score and health on screen
         scoreText.text = "Score: " + score;
-        healthText.text = "Health: " + health;
+        healthText.text = "x " + health;
+
         // tracks the number of times drbc has died
         if(bcmode)
         {
             if(health <= 0)
             {
                 BCdeathCount++;
-                health = 3; //max health
+                health = maxHealth;
             }
         }
+
+        // need to add a you died screen
     }
 
     // updates the score by adding points
@@ -59,8 +69,14 @@ public class GameManager : MonoBehaviour
     // updates the health variable to reflect taking damage
     public static void UpdateHealth(int value)
     {
-        health -= value;
+        // health is capped at 0 and maxHealth
+        if(value < 0 && health < maxHealth)
+        {
+            health -= value;
+        }
+        else if(value > 0 && health > 0)
+        {
+            health -= value;
+        }
     }
-
-    // add ongui for health and score display
 }
--- Assets/src/Jordan/Item.cs
+++ Assets/src/Jordan/Item.cs
@@ -1,3 +1,12 @@
+/**
+ * @file    Item.cs
+ * @date    09-10-22
+ * @class   CS 383
+ * @project 2D-platformer
+ * @brief   This file handles how items interact.
+ * 
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +15,7 @@ public class Item : MonoBehaviour
 {
     public int points = 1; // how much to add to score
     public int damage = 0; // how much to detract from health
-    // think about separate classes for above
-    // look into instances
+    
 
     // Start is called before the first frame update
     void Start()
@@ -24,11 +32,8 @@ public class Item : MonoBehaviour
     // when collided with, update score/health and disappear
     void OnTriggerEnter2D(Collider2D other)
     {
-        // call update score instead?
         GameManager.UpdateScore(points);
         GameManager.UpdateHealth(damage);
-        // health -= damage;
-        // call update health
 
         Destroy(gameObject);
 
